11fc7d553021fc3463329a02c04bb4cd1d478b52
Christof Belisle <Christof@jazab.com>
2021-06-12 00:12:59 -0400

    Level Completion and Loss

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndZone : MonoBehaviour
+public class DeathZone : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +14,13 @@ public class EndZone : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("We Hitting Good");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    private void OnTriggerEnter(Collision collision)
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
