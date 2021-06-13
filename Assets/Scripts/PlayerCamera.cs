608f6f7c408ab0403ca7cddca55f25a0ba37a9ca
Christof Belisle <Christof@jazab.com>
2021-06-12 21:35:08 -0400

    Level 3

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -7,7 +7,6 @@ public class PlayerCamera : MonoBehaviour {
     public float zoomSpeed = 1;
     public Transform target;
     float mouseX, mouseY;
-    public Transform obstruction;
     public LayerMask cameraLayer;
 
 
@@ -15,7 +14,6 @@ public class PlayerCamera : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        obstruction = target;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -25,7 +23,6 @@ public class PlayerCamera : MonoBehaviour {
     {
         //transform.position = new Vector3(target.position.x, target.position.y, target.position.z - 4.5f);
         CamControl();
-        ViewObstructed();
     }
 
     void CamControl()
@@ -41,33 +38,5 @@ public class PlayerCamera : MonoBehaviour {
 
 
         target.rotation = Quaternion.Euler(mouseY, mouseX, 0f);
-    }
-
-    void ViewObstructed()
-    {
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, target.position - transform.position, out hit, 4.5f))
-        {
-            if (hit.collider.gameObject.tag != "Player")
-            {
-                obstruction = hit.transform;
-                obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
-
-                //determines if the camera should hide the wall or move the camera closer
-                if (Vector3.Distance(obstruction.position, transform.position) >= 3f && Vector3.Distance(transform.position, target.position) >= 1.5f)
-                {
-                    transform.Translate(Vector3.forward * zoomSpeed * Time.deltaTime);
-                }
-            }
-            else if (obstruction.gameObject.layer != 9)
-            {
-                obstruction.gameObject.GetComponent<MeshRenderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
-                if (Vector3.Distance(transform.position, target.position) < 4.5f)
-                {
-                    transform.Translate(Vector3.back * zoomSpeed * Time.deltaTime);
-                }
-            }
-        }
     }
 }
